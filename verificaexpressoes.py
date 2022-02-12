db05f19f45b51e45194d24b33dfe99b07c1d0c02
viniyan <86854657+viniyan@users.noreply.github.com>
2022-02-12 14:27:59 -0300

    UPDATE
    adição de alguns prints dentro dos if

--- verificaexpressoes.py
+++ verificaexpressoes.py
@@ -1,8 +1,3 @@
-import json
-
-lista = ''' {"x": [{"id": 1, "texto": "Em primeiro lugar, a forma atual de ensino, em que o aluno é obrigado a sentar-se em intervalos determinados pelos superiores, forma os adultos que levam essa forma de produção para o ofício. Logo, baseado no que foi dito, vale citar o filósofo Pitágoras, que explica que é melhor educar bem as crianças do que ter que reeducá-las como adultos. Assim, os maus hábitos adquiridos na infância podem gerar, nos adultos, muitas complicações, já que dentro da sala de aula, a movimentação dos alunos pelo ambiente é repudiada e muitas vezes com consequências. Como consequência, a doutrinação do modelo educacional não atende aos paradigmas de formação de um adulto atento à saúde ocupacional."},
-{ "id": 4, "texto": "Perante os argumentos citados, é alarmante o número de indivíduos que crescem traumatizados, muitas vezes sem tratamento adequado, durante ou após a infância, desenvolvendo uma realidade de mundo inquietante. Por conseguinte, é relevante adicionar que, sendo a cultura - associada às relações e experiências de vida do cidadão - um meio pelo qual se cria a construção da realidade na consciência dos jovens, a realidade que está sendo criada pelas vítimas pode refletir na decadência da sociedade brasileira, em termos de segurança, educação e até cultural."}]}'''
-
 def verificaExpressoes(lista):
     x = json.loads(lista)
     y = x['x']
@@ -26,6 +21,7 @@ def verificaExpressoes(lista):
                         cont += 2
                         b.append('///')
                         b.append(i)
+                        print(b)
             else:
                 c = a.split('?')
                 cont += 2
@@ -35,7 +31,6 @@ def verificaExpressoes(lista):
 
         elif '!' in a:
             if cont == 0:              #não possui . ou ?
-                cont += 7
                 d = a.split('!')
                 print(d)
             if cont == 1:              #possuía . e foi criada a lista sem
@@ -45,6 +40,7 @@ def verificaExpressoes(lista):
                         cont += 3
                         b.append('///')
                         b.append(i)
+                        print(b)
             if cont == 2:              #possuía ? e foi criada a lista sem
                 f = a.split('!')
                 for i in f:
@@ -52,6 +48,7 @@ def verificaExpressoes(lista):
                         cont += 3
                         c.append('///')
                         c.append(i)
+                        print(c)
             if cont == 3:                 #possuía . e ? e foi criada a lista sem
                 g = a.split('!')
                 for i in g:
@@ -59,3 +56,4 @@ def verificaExpressoes(lista):
                         cont += 3
                         b.append('///')
                         b.append(i)
+                        print(b)
